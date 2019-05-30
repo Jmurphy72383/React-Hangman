bd1ea7a2df3648fc577b75cb39bb238e8cd26a32
JMurphy <murph_iraq@yahoo.com>
2019-05-30 18:43:14 -0400

    Updates

---
+++ precache-manifest.3213785e659ef369993fe03f17d8d649.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ac7abd9764548bbbe0b5693bc4f4d7",
+    "url": "/React-Hangman/index.html"
+  },
+  {
+    "revision": "a89410186d07431c90ef",
+    "url": "/React-Hangman/static/css/main.8b9fe31e.chunk.css"
+  },
+  {
+    "revision": "c76acbd8c3450c465efd",
+    "url": "/React-Hangman/static/js/2.4cc8969d.chunk.js"
+  },
+  {
+    "revision": "a89410186d07431c90ef",
+    "url": "/React-Hangman/static/js/main.292a518c.chunk.js"
+  },
+  {
+    "revision": "0d894d6e3c1eef21888d",
+    "url": "/React-Hangman/static/js/runtime~main.44d46a2d.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/React-Hangman/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/React-Hangman/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/React-Hangman/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/React-Hangman/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/React-Hangman/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/React-Hangman/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/React-Hangman/static/media/6.c80394c2.jpg"
+  },
+  {
+    "revision": "6af6233fe482302c26d3668cb77cd004",
+    "url": "/React-Hangman/static/media/got_faces.6af6233f.jpg"
+  }
+]);
